5c999a7806c095d1c804f0655a0f898c480fb23d
Muhammad-Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
2024-05-28 05:53:03 +0200

    update a test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,10 @@
-import { core } from 'web3';
+import { Web3 } from 'web3';
 import { ZkSyncPlugin } from '../src';
 
 describe('ZkSyncPlugin tests', () => {
 	it('should register ZkSync plugin on Web3Context instance', () => {
-		const web3Context = new core.Web3Context('http://127.0.0.1:8545');
-		web3Context.registerPlugin(new ZkSyncPlugin());
-		expect(web3Context.zkSync).toBeDefined();
+		const web3 = new Web3('http://127.0.0.1:8545');
+		web3.registerPlugin(new ZkSyncPlugin());
+		expect(web3.zkSync).toBeDefined();
 	});
 });
